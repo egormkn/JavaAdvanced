3370adc490908924458e725d0d099e61a6737fcd
Egor Makarenko <egormkn@yandex.ru>
2017-02-12 22:12:31 +0300

    Added run script and submodule

---
+++ run.bat
@@ -0,0 +1,1 @@
+java -Xmx20M -cp tests/lib/hamcrest-core-1.3.jar;tests/lib/junit-4.11.jar;tests/artifacts/WalkTest.jar;./out/production/JavaAdvanced info.kgeorgiy.java.advanced.walk.Tester RecursiveWalk ru.ifmo.ctddev.makarenko.walk.RecursiveWalk
