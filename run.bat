a13edbdf6c3372a7501d69bb1902367ef554b47b
Egor Makarenko <egormkn@yandex.ru>
2017-02-12 22:22:13 +0300

    *.bat runner

--- run.bat
+++ run.bat
@@ -1,1 +1,7 @@
-java -Xmx20M -cp tests/lib/hamcrest-core-1.3.jar;tests/lib/junit-4.11.jar;tests/artifacts/WalkTest.jar;./out/production/JavaAdvanced info.kgeorgiy.java.advanced.walk.Tester RecursiveWalk ru.ifmo.ctddev.makarenko.walk.RecursiveWalk
+@ECHO off
+IF "%1" == "walk" (
+    java -Xmx20M -cp tests/lib/hamcrest-core-1.3.jar;tests/lib/junit-4.11.jar;tests/artifacts/WalkTest.jar;./out/production/JavaAdvanced info.kgeorgiy.java.advanced.walk.Tester Walk ru.ifmo.ctddev.makarenko.walk.Walk
+)
+IF "%1" == "recursivewalk" (
+    java -Xmx20M -cp tests/lib/hamcrest-core-1.3.jar;tests/lib/junit-4.11.jar;tests/artifacts/WalkTest.jar;./out/production/JavaAdvanced info.kgeorgiy.java.advanced.walk.Tester RecursiveWalk ru.ifmo.ctddev.makarenko.walk.RecursiveWalk
+)
