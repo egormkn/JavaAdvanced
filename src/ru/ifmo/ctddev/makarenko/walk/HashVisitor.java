a532db3acafc523b71393f4942ee0bc155d4782e
Egor Makarenko <egormkn@yandex.ru>
2017-02-13 00:17:22 +0300

    Switched to SimpleFileVisitor

--- src/ru/ifmo/ctddev/makarenko/walk/HashVisitor.java
+++ src/ru/ifmo/ctddev/makarenko/walk/HashVisitor.java
@@ -3,20 +3,15 @@ package ru.ifmo.ctddev.makarenko.walk;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.FileVisitResult;
-import java.nio.file.FileVisitor;
 import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 
-public class HashVisitor implements FileVisitor<Path> {
+public class HashVisitor extends SimpleFileVisitor<Path> {
     private final BufferedWriter writer;
 
     public HashVisitor(BufferedWriter writer) {
         this.writer = writer;
-    }
-
-    @Override
-    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
-        return FileVisitResult.CONTINUE;
     }
 
     @Override
@@ -28,11 +23,6 @@ public class HashVisitor implements FileVisitor<Path> {
     @Override
     public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
         writer.append(Utils.defaultHash).append(' ').append(file.toString()).append(System.lineSeparator());
-        return FileVisitResult.CONTINUE;
-    }
-
-    @Override
-    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
         return FileVisitResult.CONTINUE;
     }
 }
