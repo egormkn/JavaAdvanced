f06ff60a6979bda8a5960d85283dddfeab423440
egormkn <egormkn@yandex.ru>
2017-03-18 15:06:56 +0300

    Test added

--- src/ru/ifmo/ctddev/makarenko/implementor/Test.java
+++ src/ru/ifmo/ctddev/makarenko/implementor/Test.java
@@ -1,9 +1,14 @@
 package ru.ifmo.ctddev.makarenko.implementor;
 
-import javax.management.ImmutableDescriptor;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 
-public abstract class Test<TEST1, TEST2, TEST3 extends Cloneable & Appendable> implements Comparable<TEST2>, List<TEST1> {
-    //ImmutableDescriptor
+public class Test {
+    public static void main(String[] args) {
+        try {
+            new ClassWriter(System.out).print(ArrayList.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
