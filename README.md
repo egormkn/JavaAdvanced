c9c8f09dcdf2da16fb6fb881805494f673bf733b
Egor Makarenko <egormkn@yandex.ru>
2017-03-28 16:02:53 +0300

    Tests updated

---
+++ README.md
@@ -0,0 +1,27 @@
+# Домашние задания к курсу «Технологии Java»
+
+- [Условия домашних заданий](http://www.kgeorgiy.info/courses/java-advanced/homeworks.html)
+- [Тесты](/tests)  
+
+### Запуск тестов
+```
+test.cmd <NAME> <SALT>
+```
+или
+```
+test.sh <NAME> <SALT>
+```
+
+Где `<NAME>`: 
+- `1_easy`
+- `1_hard`
+- `2_easy`
+- `2_hard`
+- `3_easy`
+- `3_hard`
+- `4_easy`
+- `4_hard`
+- `6_easy`
+- `6_hard`
+- `7_easy`
+- `7_hard`
--- builddoc.bat
+++ builddoc.bat
@@ -1,4 +1,4 @@
 @ECHO off
-SET DOCS="doc"
-@RD /S /Q %DOCS%
-javadoc  -d %DOCS% src\ru\ifmo\ctddev\makarenko\implementor\package-info.java src\ru\ifmo\ctddev\makarenko\implementor\Implementor.java src\ru\ifmo\ctddev\makarenko\implementor\ClassWriter.java -link http://docs.oracle.com/javase/8/docs/api -private -classpath tests\lib\hamcrest-core-1.3.jar;tests\lib\junit-4.11.jar;tests\lib\quickcheck-0.6.jar;tests\artifacts\JarImplementorTest.jar
+SET FOLDER="doc"
+@RD /S /Q %FOLDER%
+javadoc -d %FOLDER% tests\java\info\kgeorgiy\java\advanced\implementor\ImplerException.java tests\java\info\kgeorgiy\java\advanced\implementor\Impler.java tests\java\info\kgeorgiy\java\advanced\implementor\JarImpler.java src\ru\ifmo\ctddev\makarenko\implementor\package-info.java src\ru\ifmo\ctddev\makarenko\implementor\Implementor.java src\ru\ifmo\ctddev\makarenko\implementor\ClassWriter.java -link http://docs.oracle.com/javase/8/docs/api -private -classpath tests\lib\hamcrest-core-1.3.jar;tests\lib\junit-4.11.jar;tests\lib\quickcheck-0.6.jar;tests\artifacts\JarImplementorTest.jar
